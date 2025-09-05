7149ce6211ad39d6296d6655ac5da8adcee3ccf6
Jbibay009 <jonathanbibay029@gmail.com>
2025-09-05 13:51:23 +0800

    Add files via upload

---
+++ BS_Activity2/Act2.php
@@ -0,0 +1,150 @@
+<html>
+<head>
+    <title> 3BSCS-1 Activity 2</title>
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+		<script src="js/jquery.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+    <h1>Activity 2</h1>
+
+    
+<div class = "container-fluid">
+    <a href="#">Messages<span class = "badge">10</span></a><br/>
+    <a href="#">Notification<span class = "badge">30</span></a><br/>
+    <button class = "btn btn-primary">Click to see notifications<span class = "badge">500</span></button>
+
+    <div>
+        <h3>Download <span class = "label label-default">Version 10.1</span></h3>
+    </div>
+</div>
+
+<br/>
+<hr/>    
+<div class  = "container-fluid">
+    <div class = "alert alert-danger fade in"><a href="#" class = "close" data-dismiss = "alert">&times;</a> Record already Exist!!</div>
+    <div class = "alert alert-success"><a href="#" class = "close" data-dismiss = "alert">&times;</a> Record already Exist!!</div>
+    <div class = "alert alert-warning"><a href="#" class = "close" data-dismiss = "alert">&times;</a> Record already Exist!!</div>
+    <div class = "alert alert-info"><a href="#" class = "close" data-dismiss = "alert">&times;</a> Record already Exist!!</div>
+</div>
+
+<br/>
+<hr/>    
+<div  class = "container-fluid">
+    <div class = "jumbotron">
+        <h1>Heading in Jumbotron</h1>
+    </div>
+
+        <h1>Heading without Jumbotron</h1>
+
+    <div class = "page header">
+        <h1>Welcome</h1>
+    </div>
+
+    <p class = "Well well-sm">This is the well</p>
+    <p class = "Well">This is the well</p>
+    <p class = "Well well-lg">This is the well</p>
+
+</div>
+
+<hr/>
+<div class = "container-fluid">
+    <span class = "glyphicon glyphicon-trash"></span>: Trash Can <br/>
+    <span class = "glyphicon glyphicon-pencil"></span>: Pencil <br/>
+    <button class = "btn btn-success"><span class = "glyphicon glyphicon-search"></span> Find</button>
+</div>
+
+<br/>
+<hr/>
+<div class = "container-fluid">
+
+    <div class = "dropdown">
+            <button class = "btn btn-primary dropdown-toggle" data-toggle = "dropdown">College<span class = "caret"></span></button>
+            <ul class = "dropdown-menu">
+               <li><a href="#">CCS</a></li>
+               <li><a href="#">CET</a></li>
+               <li><a href="#">CBA</a></li>
+               <li class = "divider"></li>
+               <li class = "dropdown-header">Exit</li>
+               <li><a href="#">LogOut</a></li>
+            </ul>       
+    </div>
+
+    <div class = "dropdown">
+            <button class = "btn btn-primary dropdown-toggle" data-toggle = "dropdown">College<span class = "caret"></span></button>
+            <ul class = "dropdown-menu dropdown-menu-right">
+               <li><a href="#">CCS</a></li>
+               <li><a href="#">CET</a></li>
+               <li><a href="#">CBA</a></li>
+               <li class = "divider"></li>
+               <li class = "dropdown-header">Exit</li>
+               <li><a href="#">LogOut</a></li>
+            </ul>       
+    </div>
+
+    <div class = "dropup">
+            <button class = "btn btn-primary dropdown-toggle" data-toggle = "dropdown">College<span class = "caret"></span></button>
+            <ul class = "dropdown-menu">
+               <li><a href="#">CCS</a></li>
+               <li><a href="#">CET</a></li>
+               <li><a href="#">CBA</a></li>
+               <li class = "divider"></li>
+               <li class = "dropdown-header">Exit</li>
+               <li><a href="#">LogOut</a></li>
+            </ul>       
+    </div>
+</div>
+    
+    <br/>
+    <br/>
+    <hr/>
+    <div class = "container-fluid">
+        <button class = "btn btn-default">Click Me </button>
+        <button class = "btn btn-primary btn-xs">Click Me </button>
+        <button class = "btn btn-info btn-lg">Click Me </button>
+        <button class = "btn btn-success btn-md">Click Me </button>
+        <button class = "btn btn-warning">Click Me </button>
+        <button class = "btn btn-danger btn-sm">Click Me </button>
+        <button class = "btn btn-default btn-link">Click Me </button>
+
+        <button class = "btn btn-danger btn-block">Click Me </button>
+        <button class = "btn btn-danger disabled">Click Me </button>
+        <button class = "btn btn-danger active">Click Me </button>
+     </div>
+     <div class = "btn-group">
+        <button class = "btn btn-info">Home</button>
+        <button class = "btn btn-info">About Us</button>
+        <button class = "btn btn-info">Contact Us</button>
+        <button class = "btn btn-info">Products</button>
+     </div>
+
+     <div class = "btn-group-vertical">
+        <button class = "btn btn-info">Home</button>
+        <button class = "btn btn-info">About Us</button>
+        <button class = "btn btn-info">Contact Us</button>
+        <button class = "btn btn-info">Products</button>
+     </div>
+     <div class = "btn-group btn-group-justified">
+            <div class = "btn-group">
+                <button class = "btn btn-success">Home</button>
+            </div>
+            <div class = "btn-group">
+                <button class = "btn btn-success">About Us</button>
+            </div>
+            <div class = "btn-group">
+                <button class = "btn btn-success">Contact us</button>
+            </div>
+            <div class = "btn-group">
+                <button class = "btn btn-success">Products</button>
+            </div>
+     </div>
+
+    <hr/>
+    <div class ="container-fluid">
+        <img src="pics/goku.jpeg">
+        <img src="pics/goku.jpeg" class = "img-rounded">
+        <img src="pics/goku.jpeg" class = "img-circle">
+        <img src="pics/goku.jpeg" class = "img-thumbnail">
+    </div>
+</body>
+</html>
